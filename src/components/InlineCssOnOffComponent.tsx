1d2be8cde0f0199de1a78f91d5f7b6776c7c9f47
Botabay <tohgl49@gmail.com>
2023-04-23 21:17:23 +0600

    for InlineCssOnOffComponent it is added a simple switching

--- src/components/InlineCssOnOffComponent.tsx
+++ src/components/InlineCssOnOffComponent.tsx
@@ -1,12 +1,34 @@
+import { useState } from "react"
 export const InlineCssOnOffComponent=()=>{
+    const [onSt,setOnSt]=useState<boolean>(false)
     const onStyle={
-
+        display:'inline-block',
+        width: '30px',
+        border:'1px solid black',
+        backgroundColor:onSt?'green':'red',
+        marginLeft:'10px'
+    }
+    const offStyle={
+        display:'inline-block',
+        width: '30px',
+        backgroundColor:!onSt?'green':'red',
+        border:'1px solid black',
+        marginLeft:'10px'
+    }
+    const switcherStyle={
+        display:'inline-block',
+        width: '10px',
+        height:'10px',
+        backgroundColor:'red',
+        borderRadius:'50%',
+        border:'1px solid black',
+        marginLeft:'10px'
     }
     return (
         <div>
-            <span>on</span>
-            <span>off</span>
-            <span></span>
+            <span style={onStyle} >on</span>
+            <span style={offStyle}>off</span>
+            <span style={switcherStyle} onClick={()=>{setOnSt(!onSt)}}></span>
         </div>
     )
 }
