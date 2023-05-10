e84ea93fe3b09b4165b5de5b6d5812b79c17069e
Botabay <tohgl49@gmail.com>
2023-05-10 11:51:52 +0600

    created first story for a rating

---
+++ src/components/Rating/Rating.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Rating } from "./Rating";
+
+export default {
+    title:' what is this,wtf',
+    component: Rating
+}
+
+export const twoStart=()=> <Rating starCount={2} onClick={g=>g}/>
