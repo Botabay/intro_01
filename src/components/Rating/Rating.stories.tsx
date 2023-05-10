0fb1a559e59af272ba0d5dedb91ee4ba1cd97a5d
Botabay <tohgl49@gmail.com>
2023-05-10 12:22:56 +0600

    stories for Rating-component is done

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-import { Rating } from "./Rating";
+import React, { useState } from "react";
+import { Rating, ratingValueType } from "./Rating";
 
 export default {
-    title:' what is this,wtf',
+    title: ' Rating stories',
     component: Rating
 }
 
-export const twoStart=()=> <Rating starCount={2} onClick={g=>g}/>
+export const Rating0 = () => <Rating starCount={0} onClick={g => g} />
+export const Rating1 = () => <Rating starCount={1} onClick={g => g} />
+export const Rating2 = () => <Rating starCount={2} onClick={g => g} />
+export const Rating3 = () => <Rating starCount={3} onClick={g => g} />
+export const Rating4 = () => <Rating starCount={4} onClick={g => g} />
+export const Rating5 = () => <Rating starCount={5} onClick={g => g} />
+
+export const ChangeRating = () => {
+    const [r, setR] = useState<ratingValueType>(0);
+    return <Rating starCount={r} onClick={setR} />
+}
