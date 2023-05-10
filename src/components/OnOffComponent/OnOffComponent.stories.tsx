3b571892b9b3cc52b7eb95682b214b338a5f96e7
Botabay <tohgl49@gmail.com>
2023-05-10 16:26:34 +0600

    used "action" in stories

--- src/components/OnOffComponent/OnOffComponent.stories.tsx
+++ src/components/OnOffComponent/OnOffComponent.stories.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { OnOffComponent } from "./OnOffComponent";
+import {action} from '@storybook/addon-actions'
 
 export default {
     title: ' OnOffComponent stories',
     component: OnOffComponent
 }
+const f=action('click')
+export const OnMode1 = () => <OnOffComponent turnedOn={true} callback={f}/>
+export const OffMode1 = () => <OnOffComponent turnedOn={false} callback={f}/>
 
 export const OnMode = () => <OnOffComponent turnedOn={true} callback={x=>x}/>
 export const OffMode = () => <OnOffComponent turnedOn={false} callback={x=>x}/>
