6952bcca5fcdad1fdaf2333ff2fa8ea00f0a2305
Botabay <tohgl49@gmail.com>
2023-05-13 22:28:41 +0600

    added a inputStories to learnDirectory, uncontrolled or controlled point

---
+++ src/stories/learnStories/input.stories.tsx
@@ -0,0 +1,12 @@
+
+export default {
+    title: 'learn input'
+}
+
+/**
+ * 
+ * uncontrolled input is not good thing for any project
+ */
+export const UncontrolledInput = ()=><input />
+
+export const ControlledInputWithFixedValue = ()=><input value={'ControlledInputWithFixedValue'}/>
