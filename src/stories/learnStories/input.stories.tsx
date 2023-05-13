828e4b8920ae3d0fd7f58252769075f7f6f879a4
Botabay <tohgl49@gmail.com>
2023-05-13 22:55:46 +0600

    added a inputStories to learnDirectory, uncontrolled or controlled, topic12,week6 is ended

--- src/stories/learnStories/input.stories.tsx
+++ src/stories/learnStories/input.stories.tsx
@@ -1,4 +1,4 @@
-
+import { useState, useRef } from 'react'
 export default {
     title: 'learn input'
 }
@@ -7,6 +7,25 @@ export default {
  * 
  * uncontrolled input is not good thing for any project
  */
-export const UncontrolledInput = ()=><input />
+export const UncontrolledInput = () => <input />
 
-export const ControlledInputWithFixedValue = ()=><input value={'ControlledInputWithFixedValue'}/>
+export const ControlledInputWithFixedValue = () => <input value={'ControlledInputWithFixedValue'} />
+
+export const TrackValueOfUncontrolledInput = () => {
+    const [value, setValue] = useState('')
+    return <>
+        <input value={value} onChange={(e) => {
+            setValue(e.currentTarget.value)
+        }} /> {value}
+    </>
+}
+
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState('')
+    const ref = useRef<HTMLInputElement>(null)
+    return <>
+        <input ref={ref} />
+        <button onClick={() => { setValue(ref.current ? ref.current.value : '') }}>get inputValue</button>
+        {value}
+    </>
+}
