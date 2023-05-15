15cf506b3576090f399bccb06bbb5c88c6f2cc5c
Botabay <tohgl49@gmail.com>
2023-05-15 21:46:08 +0600

    week6,topic14, homework, pseudoselect,step2,workable action

--- src/stories/learnStories/pseudoSelect.stories.tsx
+++ src/stories/learnStories/pseudoSelect.stories.tsx
@@ -1,32 +1,42 @@
+import { useState, useRef } from 'react'
+import { action } from '@storybook/addon-actions'
+
 export default {
     title: 'learn/PseudoSelect stories',
     // component: Accordion
 }
 
-type ItemType={
-    title:string
-    value:any
+type ItemType = {
+    title: string
+    value: any
 }
 
 type SelectPropsType = {
-    value:any
-    onChange: (v:any)=>void
-    items:ItemType[]
+    value: any
+    onChange: (v: any) => void
+    items: ItemType[]
 }
 
-const Select=({value,onChange,items}:SelectPropsType)=>{
+const Select = ({ value, onChange, items }: SelectPropsType) => {
     return (
         <div>
             <p>{value}</p>
             <div>
-                {items.map(el=>(
-                    <p key={el.value} onClick={()=>{onChange(el.title)}}>{el.title}</p>
+                {items.map(el => (
+                    <p key={el.value} onClick={() => { onChange(el.title) }}>{el.title}</p>
                 ))}
             </div>
         </div>
     )
 }
 
-export function SelectTest(){
-    return <Select value={'select title'} onChange={()=>console.log('ddd')} items={[{title:'option',value:22}]}/>
+const actionCallback=action('this message from the storybook')
+
+const items = [
+    { title: 'floor', value: '1' },
+    { title: 'door', value: '2' },
+]
+
+export function SelectTest() {
+    return <Select value={'select title'} onChange={actionCallback} items={items} />
 }
