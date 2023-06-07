6692191096768d86d8a1dc6d05818a400f99f3b9
Botabay <tohgl49@gmail.com>
2023-06-07 12:22:12 +0600

    week10 is done,without homework

--- src/stories/learnStories/useEffect.stories.tsx
+++ src/stories/learnStories/useEffect.stories.tsx
@@ -7,6 +7,7 @@ export default {
 /**
  * 
  * useEffect like sipmle
+ * useEffect at clock
  */
 
 const functionWithHardCalculations = (): number => {
@@ -16,11 +17,32 @@ const functionWithHardCalculations = (): number => {
 
 export const Component = () => {
     const [value, setValue] = useState<number>(functionWithHardCalculations)
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect is worked');
-        
     })
     return <>
         <button onClick={() => setValue(value + 1)}>{value}</button>
+    </>
+}
+
+export const Clock = () => {
+    const [value, setValue] = useState(new Date())
+    useEffect(() => {
+        // setTimeout(()=>setValue(new Date()),1000)
+        // при уходе из этой компоненты setInterval продолжает работать,
+        // утечка памяти, поэтому нужно очистить через clearInterval
+        const id = setInterval(() => {
+            console.log('bad ticking');
+            setValue(new Date());
+            // clearInterval(id); here is it does not work
+        }, 1000)
+        return ()=>clearInterval(id);//good
+    }, [])
+    return <>
+        <span>{value.getHours()}</span>
+        :
+        <span>{value.getMinutes()}</span>
+        :
+        <span>{value.getSeconds()}</span>
     </>
 }
